027e37749cd81897d1a4a2f51605b6a9f398dcc7
Aline de Souza Montanhas <alinesmontanhas@gmail.com>
2024-08-29 11:49:08 -0300

    Add files via upload

---
+++ 04.nome-idade.py
@@ -0,0 +1,16 @@
+# atribuição de variáveis
+nome = "aline"
+idade = 16
+altura = 1.72
+
+# exibir mensagem a tela 
+print ("nome: ", nome, "idade: ", idade, "altura: ", altura)
+
+# formatação com f-string
+print (f"nome:  {nome}  idade:  {idade}  altura:  {altura}")
+
+nome = input ("digite o seu nome: ")
+idade = int (input ("digite a sua idade: "))
+altura = float (input ("digite a sua altura: "))
+
+print (f"nome:  {nome}  idade:  {idade}  altura:  {altura}")
