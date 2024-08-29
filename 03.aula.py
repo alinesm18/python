613da5d691ee305f922bbca7c3596fb379c1dddf
Aline de Souza Montanhas <alinesmontanhas@gmail.com>
2024-08-29 11:48:49 -0300

    Add files via upload

---
+++ 03.aula.py
@@ -0,0 +1,16 @@
+# váriaveis em python
+# não há necessidade de declará-las
+# basta criar no meio do algoritmo com a atribuição
+
+teste = "liline" 
+print (teste) # imprime a string "line"
+print (type (teste)) # imprime o tipo de variável, string 
+teste = 10
+print (teste) # imprime o número 10
+print (type (teste)) # imprime o tipo da variável, que é int
+teste = 1.72
+print (teste) # imprime o número 1.72
+print (type (teste)) # imprime o tipo de variável, que é float
+teste = True
+print (teste) # imprime o valor true
+print (type (teste)) # imprime o tipo da variável, que é bool 
