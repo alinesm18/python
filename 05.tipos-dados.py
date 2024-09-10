72474fb2c602712c95711d76c4f8f13516f37a2e
Aline de Souza Montanhas <alinesmontanhas@gmail.com>
2024-09-10 11:00:48 -0300

    Add files via upload

---
+++ 05.tipos-dados.py
@@ -0,0 +1,17 @@
+# tipos de dados em python
+# inteiros
+
+inteiro = 10
+print (inteiro)
+
+# floats
+float = 10.5
+print (float)
+
+# strings 
+string = "olá, mundo!"
+print (string)
+
+# booleanos
+booleano = True 
+print (booleano)
