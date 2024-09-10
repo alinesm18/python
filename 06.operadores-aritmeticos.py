245b8e4e8c937904770d3376d47c021e4cd72310
Aline de Souza Montanhas <alinesmontanhas@gmail.com>
2024-09-10 11:19:55 -0300

    Add files via upload

---
+++ 06.operadores-aritmeticos.py
@@ -0,0 +1,29 @@
+# operadores aritmeticos
+
+# soma 
+soma = 10 + 5
+print (soma)
+
+# subtração 
+subtracao = 10 - 5
+print (subtracao)
+
+# multiplicação 
+multiplicacao = 10 * 5
+print (multiplicacao)
+
+# divisão 
+divisao = 10 / 5
+print (divisao)
+
+# divisão inteira 
+divisao_inteira = 10 // 5
+print (divisao_inteira)
+
+# exponenciação 
+exponenciacao = 10 ** 5
+print (exponenciacao)
+
+# módulo / resto da divisão
+modulo = 10 % 5
+print (modulo)
