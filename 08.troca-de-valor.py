7b5455f2afb3e4aeba0678a6f91a8102e3f977cd
Aline de Souza Montanhas <alinesmontanhas@gmail.com>
2024-09-12 10:53:05 -0300

    Update 08.troca-de-valor.py

--- 08.troca-de-valor.py
+++ 08.troca-de-valor.py
@@ -7,4 +7,9 @@ print (f"valores antes da troca: A = {A}, B = {B}")
 # troca de valores 
 A,B = B,A 
 
+# outra opção:
+# temporaria = A 
+# A = B
+# B = temporaria
+
 print (f"valores após a troca: A = {A}, B = {B}")
