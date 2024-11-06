990318290bfa2f340e509cb90bce9c2854e79c21
Aline de Souza Montanhas <alinesmontanhas@gmail.com>
2024-11-06 10:52:14 -0300

    Add files via upload

---
+++ 27.nome-nota.py
@@ -0,0 +1,12 @@
+# solicite os 5 números aos usuários
+alunos = []
+notas = []
+
+for i in range(5):
+    aluno = input("digite o nome do aluno: ")
+    nota = int(input("digite a nota do aluno: "))
+    alunos.append(aluno)
+    notas.append(notas)
+
+for i in range(5):
+    print(f"o aluno {alunos[i]} tirou nota {notas[i]}!")
